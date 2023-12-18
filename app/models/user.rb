fbffea373e4e464dade8b9d876c8da781a2ead67
Yusei Nagasawa <yusei.nagasawa@techouse.jp>
2023-12-18 15:08:16 +0900

    Add validation for length of attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
+  validates :name, length: { maximum: 50 }
+  validates :email, length: { maximum: 255 }
 end
