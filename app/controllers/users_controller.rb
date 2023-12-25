71b75215551ab69bcc7244bf242a9f7cdf24f23c
Yusei Nagasawa <yusei.nagasawa@techouse.jp>
2023-12-25 12:40:36 +0900

    Redirect to user page when signed up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      puts 'User created'
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
