2e72ad4dac445929e509f5d0d8606e440dce9c1b
Yusei Nagasawa <yusei.nagasawa@techouse.jp>
2023-12-26 13:16:15 +0900

    Add log in feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      p 'user authenticated'
+      reset_session
+      log_in user
+      redirect_to user
     else
-      flash[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
     end
   end
