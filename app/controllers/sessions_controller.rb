c2a175f5b6ee247c0c92b97b8008c301263e5160
Yusei Nagasawa <yusei.nagasawa@techouse.jp>
2023-12-26 11:36:03 +0900

    Add create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      p 'user authenticated'
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
+
+  def destroy; end
 end
