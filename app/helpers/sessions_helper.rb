37994f33cef927e8b69d6d5de794f9c865713c9d
Yusei Nagasawa <yusei.nagasawa@techouse.jp>
2023-12-26 13:30:43 +0900

    Add current_user helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,10 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  def current_user
+    return unless session[:user_id]
+
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
