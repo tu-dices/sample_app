101d3b88006aba6f2743616f7aac66597d4cbb4b
Yusei Nagasawa <yusei.nagasawa@techouse.jp>
2024-01-21 22:23:19 +0900

    Add test for password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -28,6 +28,7 @@ class PasswordResetsController < ApplicationController
       @user.forget
       reset_session
       log_in @user
+      @user.update_attribute(:reset_digest, nil)
       flash[:success] = 'Password has been reset.'
       redirect_to @user
     else
