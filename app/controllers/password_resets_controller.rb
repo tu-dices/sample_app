5a4658fe55bf918d0a0a74f63c41b5eb7be77c75
Yusei Nagasawa <yusei.nagasawa@techouse.jp>
2024-01-21 17:47:17 +0900

    Add edit view

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+
   def new; end
 
   def create
@@ -15,4 +18,16 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit; end
+
+  private
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def valid_user
+    return if @user&.activated? && @user&.authenticated?(:reset, params[:id])
+
+    redirect_to root_url
+  end
 end
