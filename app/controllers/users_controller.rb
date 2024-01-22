0f8254635da314310d03c64dbecf663281ef62a5
Yusei Nagasawa <yusei.nagasawa@techouse.jp>
2024-01-22 13:41:33 +0900

    Add routes for microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,14 +54,6 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-  def logged_in_user
-    return if logged_in?
-
-    store_location
-    flash[:danger] = 'Please log in.'
-    redirect_to login_url, status: :see_other
-  end
-
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url, status: :see_other) unless current_user?(@user)
